1d6875788d4b0cb71c1ea5a26d56133deda47bfa
asepsaepudin <asepsaepudinwork@gmail.com>
2026-04-12 17:48:41 +0700

    Update mail.blade.php
    ganti icon

--- resources/views/components/icons/mail.blade.php
+++ resources/views/components/icons/mail.blade.php
@@ -1,9 +1,4 @@
-<svg {{ $attributes->merge(['viewBox' => '0 -2.5 20 20', 'version' => '1.1', 'xmlns' => 'http://www.w3.org/2000/svg', 'xmlns:xlink' => 'http://www.w3.org/1999/xlink', 'class' => 'w-6 h-6']) }}>
-    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g transform="translate(-340.000000, -922.000000)" fill="currentColor">
-            <g transform="translate(56.000000, 160.000000)">
-                <path d="M294,774.474 L284,765.649 L284,777 L304,777 L304,765.649 L294,774.474 Z M294.001,771.812 L284,762.981 L284,762 L304,762 L304,762.981 L294.001,771.812 Z"></path>
-            </g>
-        </g>
-    </g>
+<svg {{ $attributes->merge(['viewBox' => '0 0 24 24', 'xmlns' => 'http://www.w3.org/2000/svg', 'class' => 'w-6 h-6']) }} fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+    <rect x="3" y="5" width="18" height="14"></rect>
+    <polyline points="7 9 12 13 17 9"></polyline>
 </svg>
