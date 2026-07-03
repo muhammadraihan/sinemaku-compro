4b16f040b9e3baafc40fad4d2eaddad0d538eb96
nabillahnasywa <nasywanabillah21@gmail.com>
2026-07-03 18:24:52 +0700

    tampilan credit

--- resources/views/film/create.blade.php
+++ resources/views/film/create.blade.php
@@ -421,41 +421,35 @@
 
                 <select name="roles[]" class="form-control">
 
-                    <option value="Director">Director</option>
-                    <option value="Writer">Writer</option>
-                    <option value="Cast">Cast</option>
-                    <option value="Producer">Producer</option>
                     <option value="Executive Producer">Executive Producer</option>
+                    <option value="Producer">Producer</option>
+                    <option value="Co_Producer">Co_Producer</option>
+                    <option value="Witers DOP">Witers DOP</option>
+                    <option value="Art Director">Art Director</option>
+                    <option value="Wardrobe">Wardrobe</option>
+                    <option value="Sound Recordist">Sound Recordist</option>
+                    <option value="Post Producer">Post Producer</option>
                     <option value="Editor">Editor</option>
-                    <option value="Music Composer">Music Composer</option>
-                    <option value="Cinematographer">Cinematographer</option>
+                    <option value="Sound Designer">Sound Designer</option>
 
                 </select>
+          <tr>
+    <td>
+        <select name="roles[]" class="form-control">
+            ...
+        </select>
+    </td>
 
-            </td>
+    <td>
+        <input type="text" name="names[]" class="form-control">
+    </td>
 
-            <td>
-
-                <input
-                    type="text"
-                    name="names[]"
-                    class="form-control">
-
-            </td>
-
-            <td class="text-center">
-
-                <button
-                    type="button"
-                    class="btn btn-danger btn-sm remove-row">
-
-                    ×
-
-                </button>
-
-            </td>
-
-        </tr>
+    <td class="text-center">
+        <button type="button" class="btn btn-danger btn-sm remove-row">
+            ×
+        </button>
+    </td>
+</tr>
 
     `);
 
