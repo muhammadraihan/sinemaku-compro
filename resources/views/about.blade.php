97186f75e81693d9feaae1c614fbb0b8cfe12a9d
nabillahnasywa <nasywanabillah21@gmail.com>
2026-07-25 18:26:12 +0700

    Update tampilan depan

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -98,12 +98,6 @@
                         <img src="../img/p.home/_ARM1263.jpg" alt="Sinemaku Hero" class="w-full h-full object-cover">
                     </div>
                 <div class="swiper-slide h-full">
-                        <img src="../img/p.home/_ARM1127.jpg" alt="Sinemaku Hero" class="w-full h-full object-cover">
-                    </div>
-                <div class="swiper-slide h-full">
-                        <img src="../img/p.home/_ARM0928.jpg" alt="Sinemaku Hero" class="w-full h-full object-cover">
-                    </div>
-                <div class="swiper-slide h-full">
                         <img src="../img/p.home/1.jpeg" alt="Sinemaku Hero" class="w-full h-full object-cover">
                     </div>
                 <div class="swiper-slide h-full">
@@ -121,9 +115,6 @@
                 <div class="swiper-slide h-full">
                         <img src="../img/p.home/PSX_20220317_145659.jpg" alt="Sinemaku Hero" class="w-full h-full object-cover">
                      </div>
-                <div class="swiper-slide h-full">
-                        <img src="../img/p.home/SEHATI SEMATI_BTS-79.jpg" alt="Sinemaku Hero" class="w-full h-full object-cover">
-                    </div>
                      <div class="swiper-slide h-full">
                         <img src="../img/p.home/5.jpg" alt="Sinemaku Hero" class="w-full h-full object-cover">
                     </div>
