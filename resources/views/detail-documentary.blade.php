32666518f3d3b72c608bbee055cbe437faf5757f
nabillahnasywa <nasywanabillah21@gmail.com>
2026-07-29 14:35:27 +0700

    commit

--- resources/views/detail-documentary.blade.php
+++ resources/views/detail-documentary.blade.php
@@ -45,10 +45,6 @@
             @i18n($documentaries, 'title')
         </h1>
 
-            <h1 class="detail-hero-title font-peckham text-[8vw] md:text-[5vw] leading-[0.95] text-white tracking-tighter uppercase font-black hero-reveal max-w-4xl" style="font-peckham;">
-                        @i18n($documentaries, 'title')
-                    </h1>
-
         @if($video_id)
             <button
                 onclick="openHeroTrailer('{{ $video_id }}')"
